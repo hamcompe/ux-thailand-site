f3d22aff9ea8e54cabbc301d5a3a0e6b5441830c
dodadoa <maddodadoa@gmail.com>
2018-11-01 06:55:19 +0700

    style foot/ more on SpeakerCard

--- src/components/Foot.js
+++ src/components/Foot.js
@@ -1,17 +1,49 @@
 import React from 'react'
 import styled from 'react-emotion'
+import { Link } from 'gatsby'
+import mq from '../../utils/media-query'
 
 const FootWrapper = styled.div`
-  width: 100%;
-  height: 100px;
-  background-color: black;
+  background-color: #282828;
+  padding: 20px 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+const ImageLogo = styled.img`
+  height: 55px;
+  ${mq.l} {
+    height: 32px;
+  }
+`
+
+const PoweredBy = styled.p`
+  font-size: 14px;
+  font-weight: 300;
+  color: var(--color_grey_super_light);
+  align-self: center;
+`
+
+const LogoWrapper = styled.div`
+  height: 55px;
+  ${mq.l} {
+    height: 32px;
+  }
 `
 
 class Foot extends React.Component {
+  logoURLWithPrefix = name => `/images/${name}`
 
   render() {
     return (
       <FootWrapper>
+        <LogoWrapper>
+          <Link to="/">
+            <ImageLogo src={this.logoURLWithPrefix('logo_UXO-white.svg')} alt="" />
+          </Link>
+        </LogoWrapper>
+        <PoweredBy>Serve and inspire by UX Thailand, 2018</PoweredBy>
       </FootWrapper>
     )
   }
